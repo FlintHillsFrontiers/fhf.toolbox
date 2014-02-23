6c50e0125adb0073d509d126043800b5cfa1c596
Jeff Terstriep <jefft@leamgroup.com>
2014-02-23 06:19:45 +0000

    Hack setup.py to include README.md instead of README.txt.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='fhf.toolbox',
       version=version,
       description="Flint Hill Frontiers Community Toolbox",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
